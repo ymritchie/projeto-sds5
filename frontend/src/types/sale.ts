b3ca7d21ab633127f507d92c2b7e77e150ab082f
Yanisley Mora Ritchie <ymritchie@gmail.com>
2021-11-05 17:21:52 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,27 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalesPages = {
+    content?: Sale[];
+    last: boolean;
+    totalElements: number;
+    totalPages: number;
+    size?: number;
+    number: number;
+    first: boolean;
+    numberOfElements?: number;
+    empty?: boolean;
+
+}
+
 export type SalesSum =  {
     sellerName: string;
     sum: number;
